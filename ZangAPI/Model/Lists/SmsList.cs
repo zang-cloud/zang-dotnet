2e9bcc96abf3bc6c63700f22bc2601a7711a5c9f
Ana Kolar <ana.kolar@enterwell.net>
2017-03-29 10:38:20 +0200

    model part1

---
+++ ZangAPI/Model/Lists/SmsList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ZangAPI.Model.Lists
+{
+    /// <summary>
+    /// List of SMS messages
+    /// </summary>
+    /// <seealso cref="ZangAPI.Model.Lists.ZangObjectsList{SmsMessage}" />
+    public class SmsList : ZangObjectsList<SmsMessage>
+    {
+        /// <summary>
+        /// Gets or sets the elements.
+        /// </summary>
+        /// <value>
+        /// The elements.
+        /// </value>
+        [JsonProperty(PropertyName = "sms_messages")]
+        public override ICollection<SmsMessage> Elements { get; set; }
+    }
+}
---
+++ ZangAPI/Model/Usage.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using ZangAPI.Model.Enums;
+
+namespace ZangAPI.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="ZangAPI.Model.BaseZangObject" />
+    public class Usage : BaseZangObject
+    {
+        /// <summary>
+        /// Gets or sets the product.
+        /// </summary>
+        /// <value>
+        /// The product.
+        /// </value>
+        [JsonProperty(PropertyName = "product")]
+        public Product Product { get; set; }
+
+        /// <summary>
+        /// Gets or sets the product identifier.
+        /// </summary>
+        /// <value>
+        /// The product identifier.
+        /// </value>
+        [JsonProperty(PropertyName = "product_id")]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the month.
+        /// </summary>
+        /// <value>
+        /// The month.
+        /// </value>
+        [JsonProperty(PropertyName = "month")]
+        public int Month { get; set; }
+
+        /// <summary>
+        /// Gets or sets the year.
+        /// </summary>
+        /// <value>
+        /// The year.
+        /// </value>
+        [JsonProperty(PropertyName = "year")]
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity.
+        /// </summary>
+        /// <value>
+        /// The quantity.
+        /// </value>
+        [JsonProperty(PropertyName = "quantity")]
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the average cost.
+        /// </summary>
+        /// <value>
+        /// The average cost.
+        /// </value>
+        [JsonProperty(PropertyName = "average_cost")]
+        public decimal AverageCost { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total cost.
+        /// </summary>
+        /// <value>
+        /// The total cost.
+        /// </value>
+        [JsonProperty(PropertyName = "total_cost")]
+        public decimal TotalCost { get; set; }
+    }
+}
