b62da1702e05ec815a71dfcb7d6b90a8fbe431ed
Ana Kolar <ana.kolar@enterwell.net>
2017-03-30 15:37:10 +0200

    Accounts, Usages and Conferences connector added

---
+++ ZangAPI/Model/Lists/ParticipantList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ZangAPI.Model.Lists
+{
+    /// <summary>
+    /// Participant list
+    /// </summary>
+    /// <seealso cref="ZangAPI.Model.Lists.ZangObjectsList{Participant}" />
+    public class ParticipantList : ZangObjectsList<Participant>
+    {
+        /// <summary>
+        /// Gets or sets the elements.
+        /// </summary>
+        /// <value>
+        /// The elements.
+        /// </value>
+        [JsonProperty(PropertyName = "participants")]
+        public override ICollection<Participant> Elements { get; set; }
+    }
+}
