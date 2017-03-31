c335ec37b1d0b2e763d0742a3014933740cb4a76
Ana Kolar <ana.kolar@enterwell.net>
2017-03-31 13:17:34 +0200

    comments added

--- ZangAPI.Tests/Parameter.cs
+++ ZangAPI.Tests/Parameter.cs
@@ -1,9 +1,24 @@
 ﻿namespace ZangAPI.Tests
 {
+    /// <summary>
+    /// Parameter
+    /// </summary>
     public class Parameter
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
         public string Value { get; set; }
     }
 }
